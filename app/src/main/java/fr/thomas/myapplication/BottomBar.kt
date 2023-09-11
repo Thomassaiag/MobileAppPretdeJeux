6911df4caa1a00a8cc1d10fbbf105df6d519c4c3
Thomassaiag <thomas.saiag@gmail.com>
2023-09-11 15:07:31 +0200

    OneGameScreen 1st draft + fakeScreen to try different screen navigation

---
+++ app/src/main/java/fr/thomas/myapplication/BottomBar.kt
@@ -0,0 +1,41 @@
+package fr.thomas.myapplication
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BottomBar(modifier: Modifier = Modifier){
+    Row (modifier= modifier
+        .fillMaxWidth()
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.ic_action_home),
+            contentDescription = "Home Button"
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_action_profil),
+            contentDescription = "Profil Button"
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_action_calendar),
+            contentDescription = "Calendar Button"
+        )
+    }
+}
+
+
+@Composable
+@Preview
+
+fun BottomBarPreview(){
+    BottomBar()
+}
